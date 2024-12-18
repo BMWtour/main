26ab728e9313f0394b761df0999ff22ea7b9c02f
laon0125 <jinu0125@gmail.com>
2024-12-19 02:26:16 +0900

    테스트용 되돌릴꺼임

---
+++ src/main/java/com/lion/docker/DockerController.java
@@ -0,0 +1,14 @@
+package com.lion.docker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DockerController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "/hello";
+    }
+
+
+}
